5634654ba8b967ab4ae791d4437ba225018a4a58
flight-LM709 <150790738+flight-LM709@users.noreply.github.com>
2023-12-07 16:26:54 +0700

    Add files via upload

--- src/OutlineWarriorScene.js
+++ src/OutlineWarriorScene.js
@@ -8,6 +8,9 @@ export default class OutlineWarriorScene extends Phaser.Scene {
 	init() {
 		this.gameHalfWidth = this.scale.width * 0.5
 		this.gameHalfHeight = this.scale.height * 0.5
+		this.startGame = false
+		this.questionText = undefined
+		this.resultText = undefined
 	}
 
 
@@ -16,30 +19,95 @@ export default class OutlineWarriorScene extends Phaser.Scene {
 		this.load.image ('tile', 'images/tile.png')
 		this.load.image ('sky', 'images/sky.png')
 		this.load.spritesheet('player', 'images/Skeleton.png', {
-            frameWidth: 80,
-            frameHeight: 80
+            frameWidth: 100,
+            frameHeight: 100
         });
 		this.load.spritesheet('enemy', 'images/Goblin.png', {
-            frameWidth: 80,
-            frameHeight: 80
+            frameWidth: 100,
+            frameHeight: 100
         });
 		this.load.audio('france', 'sfx/france.mp3')
+		this.load.image('start-btn', 'images/start_button.png')
 	}
 
 	create() {
 				this.add.image(240, 320, 'background')
 		const sky = this.add.image(240, 145, 'sky')
-		const tile = this.physics.add.staticImage(240, sky.height- 30,
+		const tile = this.physics.add.staticImage(250, sky.height+ 35,
 			'tile')
 			this.player = this.physics.add.sprite(
 				this.gameHalfWidth - 150,
 				this.gameHalfHeight - 200, 'player'
 			)
 			.setBounce(0.2)
-			.setOffset (-20, -10)
+			.setOffset (0, -0)
+			//enemy
+			this.physics.add.collider (this.player, tile)
+			this.enemy = this.physics.add.sprite(
+				this.gameHalfWidth + 150,
+				this.gameHalfHeight - 200, 'enemy'
+			)
+			.setBounce(0.2)
+			.setOffset (0, -0)
+			.setFlipX(true)
+			this.physics.add.collider (this.enemy, tile)
+			this.createAnimation()
+			let start_button = this.add.image (this.gameHalfWidth, this.gameHalfHeight +181, 'start-btn') .setInteractive()
+			start_button.on ('pointerdown', () => {
+				this.gameStart()
+				start_button.destroy()
+			}, this) 
 	}
 
-	update() {
+	update(time) {
 
 	}
+	createAnimation() {
+		//player animations
+     this.anims.create({
+		key: 'player-standby',
+		frames: this.anims.generateFrameNumbers('player',
+		{ start: 0, end: 0 }),
+		frameRate: 10,
+		repeat: -1
+	})
+	this.anims.create({
+		key: 'player-attack',
+		frames: this.anims.generateFrameNumbers('player',
+		{ start: 0, end: 5 }),
+		frameRate: 10
+	})
+	//enemy animations
+this.anims.create({
+	key: 'enemy-standby',
+	frames: this.anims.generateFrameNumbers('enemy',
+	{ start: 0, end: 0 }),
+	frameRate: 10,
+	repeat: -1
+	})
+	this.anims.create({
+	key: 'enemy-attack',
+	frames: this.anims.generateFrameNumbers('enemy',
+	{ start: 0, end: 11 }),
+	frameRate: 10
+	})
+	}
+	gameStart(){
+		this.startGame = true
+		this.player.anims.play('player-standby', true)
+		this.enemy.anims.play('enemy-standby', true)
+		this.resultText = this.add.text(this.gameHalfWidth,200,
+		'0', { fontSize :'32px', fill: '#000'})
+		this.questionText = this.add.text(this.gameHalfWidth,100,
+		'0', { fontSize : '32px', fill: '#000' })
+		this.createButtons()
+		this.input.on('gameobjectdown', this.addNumber, this)
+		this.generateQuestion()
+		this.countdown = this.time.addEvent({
+			delay:1000,
+			callback:this.gameOver,
+			callbackScope: this,
+			loop: true
+		})
+		}
 }
